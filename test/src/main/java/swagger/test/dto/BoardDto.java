9143f0699a651d622fadbb29dbc865f1e12d9ace
kingjini <kingjini@naver.com>
2024-05-30 17:26:11 +0900

    dto 어노테이션 추가

--- test/src/main/java/swagger/test/dto/BoardDto.java
+++ test/src/main/java/swagger/test/dto/BoardDto.java
@@ -1,5 +1,6 @@
 package swagger.test.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,10 +10,19 @@ import lombok.*;
 @AllArgsConstructor
 public class BoardDto {
 
+    @Schema(description = "게시물의 고유 ID", example = "1")
     private int idx;
+
+    @Schema(description = "게시물 제목", example = "Sample Title")
     private String title;
+
+    @Schema(description = "게시물 내용", example = "This is a sample content.")
     private String contents;
+
+    @Schema(description = "게시물 이미지 URL", example = "http://example.com/image.jpg")
     private String image;
+
+    @Schema(description = "삭제 여부", example = "N")
     private String delYn;
 
     @Getter
@@ -21,6 +31,8 @@ public class BoardDto {
     @ToString
     @AllArgsConstructor
     public static class req {
+
+        @Schema(description = "게시물의 고유 ID", example = "1")
         private int idx;
     }
 
@@ -30,9 +42,17 @@ public class BoardDto {
     @ToString
     @AllArgsConstructor
     public static class req2 {
+
+        @Schema(description = "게시물 제목", example = "Sample Title")
         private String title;
+
+        @Schema(description = "게시물 내용", example = "This is a sample content.")
         private String contents;
+
+        @Schema(description = "게시물 이미지 URL", example = "http://example.com/image.jpg")
         private String image;
+
+        @Schema(description = "삭제 여부", example = "N")
         private String delYn;
     }
 
@@ -42,9 +62,17 @@ public class BoardDto {
     @ToString
     @AllArgsConstructor
     public static class res {
+
+        @Schema(description = "게시물 제목", example = "Sample Title")
         private String title;
+
+        @Schema(description = "게시물 내용", example = "This is a sample content.")
         private String contents;
+
+        @Schema(description = "게시물 이미지 URL", example = "http://example.com/image.jpg")
         private String image;
+
+        @Schema(description = "삭제 여부", example = "N")
         private String delYn;
     }
 }
